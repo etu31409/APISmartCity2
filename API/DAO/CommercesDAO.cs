fcb749db711be620167ae08af8b38e90b45f0e43
Francois <etu35530@student.henallux.be>
2018-12-03 11:11:49 +0100

    Ajout de commerces de test

--- API/DAO/CommercesDAO.cs
+++ API/DAO/CommercesDAO.cs
@@ -6,7 +6,6 @@ namespace APISmartCity.DAO
     {
         private List<Commerce> commerces = new List<Commerce>(){};
         private Address address = new Address("Rue de l'ange", 5000, 13);
-        private List<string> moyensPayements = new List<string>(){};
 
         public CommercesDAO()
         {   
@@ -15,6 +14,11 @@ namespace APISmartCity.DAO
         public List<Commerce> GetCommerces(){
             commerces = new List<Commerce>();
             commerces.Add(new Commerce(12,"Commerce de test",address));
+            commerces.Add(new Commerce(16,"Deuxième commerce de test",address));
+            commerces.Add(new Commerce(3,"Troisième commerce de test",address));
+            commerces.Add(new Commerce(4,"Quatrième commerce de test",address));
+            commerces.Add(new Commerce(5,"Cinquième commerce de test",address));
+            commerces.Add(new Commerce(6,"Sixième commerce de test",address));
             return commerces;
         }
     }
