15bb5ef3bfe0284f0c166e8d474292d236d2005d
Francois <etu35530@student.henallux.be>
2018-12-06 13:44:58 +0100

    Ajout de appSettings pour la connection string mais pas implémentation

---
+++ API/DAO/DesignTimeContextFactory.cs
@@ -0,0 +1,26 @@
+using System;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using APISmartCity.Model;
+
+namespace APISmartCity.DAO
+{
+    public class DesignTimeContextFactory : IDesignTimeDbContextFactory<SCNConnectDBContext>
+    {
+        private const string CONNECTION_STRING_CONFIG_KEY = "DesignTimeConnectionString";
+        readonly string connectionString;
+        public DesignTimeContextFactory()
+        {
+            var helper = new ConfigurationHelper(CONNECTION_STRING_CONFIG_KEY);
+            connectionString = helper.GetConnectionString();
+        }
+        public SCNConnectDBContext CreateDbContext(string[] args)
+        {
+            DbContextOptionsBuilder<SCNConnectDBContext> builder = new DbContextOptionsBuilder<SCNConnectDBContext>();
+            builder.UseSqlServer(connectionString);
+            return new SCNConnectDBContext(builder.Options);
+        }
+    }
+}
