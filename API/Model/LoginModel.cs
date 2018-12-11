51341294e7a28e7d33ab57bdfb514928061b75a0
Francois <etu35530@student.henallux.be>
2018-12-11 15:33:34 +0100

    Login model -> required

--- API/Model/LoginModel.cs
+++ API/Model/LoginModel.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace APISmartCity.Model
 {
     public class LoginModel
     {
+        [Required]
         public string Username {get; set; }
+        [Required]
         public string Password {get; set; }
     }
 }
