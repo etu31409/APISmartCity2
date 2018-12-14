174bbdf6535ef90136131dc1463e5608aaec29a3
pozzi nicolas <etu31409@student.henallux.be>
2018-12-14 20:12:22 +0100

    petites modif script sql (ajout de meilleur produit phare + numero)

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -47,20 +47,20 @@ CREATE TABLE Commerce(
 	FOREIGN KEY (IdPersonne) REFERENCES Users(IdUser),
 	FOREIGN KEY (IdCategorie) REFERENCES Categorie(IdCategorie)
 );
-/*Il faut d'abord insérer les catégories pour que ca marche !!!*/
+/*Il faut d'abord insï¿½rer les catï¿½gories pour que ca marche !!!*/
 
-INSERT INTO Commerce (NomCommerce, Rue, Numero, Description, ProduitPhare, AdresseMail, IdPersonne, IdCategorie) VALUES ('Burger King', 'Rue du Burger King', 26, 'Restauration rapide', 'Whooper', 'info@Burger-King.com',1,1);
-INSERT INTO Commerce (NomCommerce, Rue, Numero, Description, ProduitPhare, AdresseMail, IdPersonne, IdCategorie) VALUES ('Quick', 'Rue du Quick', 123, 'Restauration rapide', 'Giant', 'info@Quick.com',2,1);
+INSERT INTO Commerce (NomCommerce, Rue, Numero, Description, ProduitPhare, AdresseMail, IdPersonne, IdCategorie, NumeroGsm, NumeroFixe) VALUES ('Burger King', 'Rue du Burger King', 26, 'Restauration rapide', 'Whooper pour seulement 4 euros !', 'info@Burger-King.com',1,1, 0473554125, 082457820);
+INSERT INTO Commerce (NomCommerce, Rue, Numero, Description, ProduitPhare, AdresseMail, IdPersonne, IdCategorie) VALUES ('Quick', 'Rue du Quick', 123, 'Restauration rapide', 'Giant, Rendu cÃ©lÃ¨bre grÃ¢ce Ã  sa sauce Giant inimitable, Ã  base de mayonnaise, de ketchup et de notes de cÃ¢pres.', 'info@Quick.com',2,1);
 INSERT INTO Commerce (NomCommerce, Rue, Numero, Description, ProduitPhare, AdresseMail, IdPersonne, IdCategorie) VALUES ('KFC', 'Rue du KFC', 17, 'Restauration rapide', 'Saut de poulet','info@KFC.com',2,1);
 INSERT INTO Commerce (NomCommerce, Rue, Numero, Description, ProduitPhare, AdresseMail, IdPersonne, IdCategorie) VALUES ('MacDo', 'Rue du MacDo', 63, 'Restauration rapide', 'Big Mac', 'info@MacDo.com',1,1);
 
 INSERT INTO Commerce (NomCommerce, Rue, Numero, Description, AdresseMail, IdPersonne, IdCategorie) VALUES ('Foot Locker', 'Rue du Foot Locker', 1, 'Magasin de chaussures', 'info@Foot-Locker.com',1,2);
-INSERT INTO Commerce (NomCommerce, Rue, Numero, Description, ProduitPhare, AdresseMail, IdPersonne, IdCategorie) VALUES ('Zara', 'Rue du Zara', 74, 'Magasin de vétements', 'Echarpe Hiver', 'info@Zara.com',1,2);
+INSERT INTO Commerce (NomCommerce, Rue, Numero, Description, ProduitPhare, AdresseMail, IdPersonne, IdCategorie) VALUES ('Zara', 'Rue du Zara', 74, 'Magasin de vï¿½tements', 'Echarpe Hiver', 'info@Zara.com',1,2);
 INSERT INTO Commerce (NomCommerce, Rue, Numero, Description, AdresseMail, IdPersonne, IdCategorie) VALUES ('Zeeman', 'Rue du Zeeman', 416, 'Plus grande chaine de magasin en belgique', 'info@Zeeman.com',2,2);
 INSERT INTO Commerce (NomCommerce, Rue, Numero, Description, ProduitPhare, AdresseMail, IdPersonne, IdCategorie) VALUES ('Sixt', 'Rue du Sixt', 50, 'Location de voitures', 'Mercedes AMG', 'info@Sixt.com',2,2);
 
 INSERT INTO Commerce (NomCommerce, Rue, Numero, Description, AdresseMail, IdPersonne, IdCategorie) VALUES ('Barnabeer', 'Rue du Barnabeer', 1, 'Plus grand bar de namur', 'info@Barnabeer.com',1,3);
-INSERT INTO Commerce (NomCommerce, Rue, Numero, Description, ProduitPhare, AdresseMail, IdPersonne, IdCategorie) VALUES ('Green Fairy', 'Rue du green', 74, 'Bar qui rend hommage a green lantern', 'Pinte de 50 à moitié prix', 'info@green-fairy.com',1,3);
+INSERT INTO Commerce (NomCommerce, Rue, Numero, Description, ProduitPhare, AdresseMail, IdPersonne, IdCategorie) VALUES ('Green Fairy', 'Rue du green', 74, 'Bar qui rend hommage a green lantern', 'Pinte de 50 ï¿½ moitiï¿½ prix', 'info@green-fairy.com',1,3);
 INSERT INTO Commerce (NomCommerce, Rue, Numero, Description, AdresseMail, IdPersonne, IdCategorie) VALUES ('Au nom de la rose', 'Rue du bar', 416, 'Bar / kot a projet', 'info@ANDLR.com',2,3);
 INSERT INTO Commerce (NomCommerce, Rue, Numero, Description, ProduitPhare, AdresseMail, IdPersonne, IdCategorie) VALUES ('Etoile', 'Rue magique', 50, 'Petit bar rustique qui propose des parties de billard', 'Chimay bleu', 'info@etoiles.com',2,3);
 
