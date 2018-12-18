3ac4d57f5cc6870ccf2a7050b5a5ca2289c5975a
Francois <etu35530@student.henallux.be>
2018-12-18 11:15:11 +0100

    Debut gestion images

--- API/Controllers/ImageController.cs
+++ API/Controllers/ImageController.cs
@@ -13,6 +13,13 @@ using CloudinaryDotNet;
 using Microsoft.AspNetCore.Http;
 using System.IO;
 using CloudinaryDotNet.Actions;
+using System.Globalization;
+using System.Text;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Logging;
+using Microsoft.Net.Http.Headers;
 
 namespace APISmartCity.Controllers
 {
@@ -21,33 +28,106 @@ namespace APISmartCity.Controllers
     [ApiController]
     public class ImageController : ControllerBase
     {
-        CloudinaryDotNet.Cloudinary _cloudinary;
-        private SCNConnectDBContext context;
-        public ImageController(SCNConnectDBContext context)
+        private CloudinaryDotNet.Cloudinary cloudinary;
+        public ImageController()
         {
-            Account account  = new Account("dtf5i3kcx","331395718795461","jAAPUe06bLnqQbogzB5nREwJBRQ"/*a cplt avec les clefs*/);
-          _cloudinary = new Cloudinary(account);        
+            Account account = new Account("dtf5i3kcx", "331395718795461", "jAAPUe06bLnqQbogzB5nREwJBRQ");
+            cloudinary = new Cloudinary(account);
         }
-        [HttpPost]
-      public IActionResult PostAsync(IFormFile file)
-      {
-          var filePath = Path.GetTempFileName();
-          if (file.Length > 0)
-          {
-              using (var stream = file.OpenReadStream())
-              {
-                  ImageUploadResult results = _cloudinary.Upload(new ImageUploadParams()
-                  {
-                      File = new FileDescription(file.Name, stream)
-                  });
-                  if (results.Error == null)
-                      return Ok(results.Uri);
-                  else
-                      return BadRequest(results.Error.Message);
-              }
-                  
-          }
-          return BadRequest("Unsupported format");
+
+        private static readonly FormOptions _defaultFormOptions = new FormOptions();
+
+        [HttpPost()]
+        public async Task<IActionResult> Post()
+        {
+            if (!MultipartRequestHelper.IsMultipartContentType(Request.ContentType))
+            {
+                return BadRequest($"Expected a multipart request, but got {Request.ContentType}");
+            }
+
+            // Used to accumulate all the form url encoded key value pairs in the 
+            // request.
+            var formAccumulator = new KeyValueAccumulator();
+            string targetFilePath = null;
+
+            var boundary = MultipartRequestHelper.GetBoundary(
+                MediaTypeHeaderValue.Parse(Request.ContentType),
+                _defaultFormOptions.MultipartBoundaryLengthLimit);
+            var reader = new MultipartReader(boundary, HttpContext.Request.Body);
+
+            var section = await reader.ReadNextSectionAsync();
+            while (section != null)
+            {
+                ContentDispositionHeaderValue contentDisposition;
+                var hasContentDispositionHeader = ContentDispositionHeaderValue.TryParse(section.ContentDisposition, out contentDisposition);
+
+                if (hasContentDispositionHeader)
+                {
+                    if (MultipartRequestHelper.HasFileContentDisposition(contentDisposition))
+                    {
+                        targetFilePath = Path.GetTempFileName();
+                        using (var targetStream = System.IO.File.Create(targetFilePath))
+                        {
+                            await section.Body.CopyToAsync(targetStream);
+
+                            Console.WriteLine($"Copied the uploaded file '{targetFilePath}'");
+                        }
+                    }
+                    else if (MultipartRequestHelper.HasFormDataContentDisposition(contentDisposition))
+                    {
+                        // Content-Disposition: form-data; name="key"
+                        //
+                        // value
+
+                        // Do not limit the key name length here because the 
+                        // multipart headers length limit is already in effect.
+                        var key = HeaderUtilities.RemoveQuotes(contentDisposition.Name);
+                        var encoding = GetEncoding(section);
+                        using (var streamReader = new StreamReader(
+                            section.Body,
+                            encoding,
+                            detectEncodingFromByteOrderMarks: true,
+                            bufferSize: 1024,
+                            leaveOpen: true))
+                        {
+                            // The value length limit is enforced by MultipartBodyLengthLimit
+                            var value = await streamReader.ReadToEndAsync();
+                            if (String.Equals(value, "undefined", StringComparison.OrdinalIgnoreCase))
+                            {
+                                value = String.Empty;
+                            }
+                            formAccumulator.Append(key.ToString(), value);
+
+                            if (formAccumulator.ValueCount > _defaultFormOptions.ValueCountLimit)
+                            {
+                                throw new InvalidDataException($"Form key count limit {_defaultFormOptions.ValueCountLimit} exceeded.");
+                            }
+                        }
+                    }
+                }
+
+                // Drains any remaining section body that has not been consumed and
+                // reads the headers for the next section.
+                section = await reader.ReadNextSectionAsync();
+            }
+         
+
+            return Ok(new {
+                FilePath=targetFilePath
+            });
+        }
+
+         private static Encoding GetEncoding(MultipartSection section)
+        {
+            MediaTypeHeaderValue mediaType;
+            var hasMediaTypeHeader = MediaTypeHeaderValue.TryParse(section.ContentType, out mediaType);
+            // UTF-7 is insecure and should not be honored. UTF-8 will succeed in 
+            // most cases.
+            if (!hasMediaTypeHeader || Encoding.UTF7.Equals(mediaType.Encoding))
+            {
+                return Encoding.UTF8;
+            }
+            return mediaType.Encoding;
         }
     }
 }
