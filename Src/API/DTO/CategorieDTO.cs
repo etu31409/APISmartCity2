efed32335427f4eb66ffd916b31753733c1ebf29
pozzi nicolas <etu31409@student.henallux.be>
2019-01-07 16:32:14 +0100

    supp attribut

--- Src/API/DTO/CategorieDTO.cs
+++ Src/API/DTO/CategorieDTO.cs
@@ -6,14 +6,7 @@ namespace APISmartCity.DTO
 {
     public partial class CategorieDTO
     {
-        public CategorieDTO()
-        {
-            Commerce = new HashSet<CommerceDTO>();
-        }
-
         public int IdCategorie { get; set; }
         public string Libelle { get; set; }
-
-        public ICollection<CommerceDTO> Commerce { get; set; }
     }
 }
